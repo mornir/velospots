e8b89c57bb56e8de0b2ac16ca67e2ffab4acd8f7
mornir <jeromepott@hotmail.com>
2020-03-25 17:51:59 +0100

    Disable caching

--- web/service-worker.js
+++ web/service-worker.js
@@ -4,14 +4,14 @@ importScripts(
 
 /* workbox.precaching.precacheAndRoute(self.__WB_MANIFEST) */
 
-if (workbox) {
+/* if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`)
 } else {
   console.log(`Boo! Workbox didn't load 😬`)
 }
-
+ */
 /* workbox.setConfig({
   debug: true
 }) */
 
-workbox.routing.registerRoute(/\.js$/, new workbox.strategies.NetworkFirst())
+/* workbox.routing.registerRoute(/\.js$/, new workbox.strategies.NetworkFirst()) */
